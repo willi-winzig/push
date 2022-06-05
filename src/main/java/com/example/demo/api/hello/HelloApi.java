9e17e17e6708cf942d8ae8d0da2a98b337b9b229
PaulF <paul.fischer@web.de>
2022-06-05 12:28:09 +0200

    Verfeinerung

---
+++ src/main/java/com/example/demo/api/hello/HelloApi.java
@@ -0,0 +1,15 @@
+package com.example.demo.api.hello;
+
+import com.example.demo.api.order.OrderRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/push")
+public class HelloApi {
+
+    @GetMapping(path = "/hello")
+    public @ResponseBody String order() {
+        return "hello";
+    }
+}
