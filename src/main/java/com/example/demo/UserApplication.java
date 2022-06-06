d4fba0aea59524610d1752497f45cf7fa08bef15
PaulF <paul.fischer@web.de>
2022-06-06 18:45:05 +0200

    h2 and exclude tomcat

--- src/main/java/com/example/demo/UserApplication.java
+++ src/main/java/com/example/demo/UserApplication.java
@@ -14,7 +14,6 @@ public class UserApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(UserApplication.class);
     }
-
     public static void main(String[] args) {
 
         SpringApplication.run(UserApplication.class, args);
